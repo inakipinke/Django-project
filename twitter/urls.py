3be314d334a373b35c67d6f9a00ff4f84cd2cf1a
inakipinke <inakipinke@gmail.com>
2017-09-28 08:15:50 -0300

    Actualización 28/09

--- twitter/urls.py
+++ twitter/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-url(r'^$', views.post_list),
+url(r'^$', views.post_list, name='post_list'),
 url(r'^register/', views.registrar, name='registrar'), 
 url(r'^login/', views.logear, name='logear'), 
+url(r'^borrar/', views.borrar_post, name='borrar_post'), 
 url(r'^postear/', views.postear, name='postear'), 
 url(r'^calendario/', views.calendario),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -9,7 +9,22 @@ from django.contrib.auth.models import User
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	post_borrar2 = request.POST.get('botoncito','')
+	print(post_borrar2)
+	print("post_borrar2")
+	if (post_borrar2 != ""):
+		postb2 = Post.objects.filter(title=post_borrar2)
+		postb2.delete()
 	return render(request, 'twitter/post_list.html', {'posts': posts})
+
+def borrar_post(request):
+	post_borrar = request.POST.get('borrarxd','')
+	if (post_borrar != ""):
+		postb = Post.objects.filter(title=post_borrar)
+		postb.delete()
+
+	return render(request, 'twitter/borrar_post.html',)
+
 
 def registrar(request):
 	nickname2 = request.POST.get('user1','')
